f77d381937feedcc10d17e4210d58143fbe4c46c
tylen <tylen@vm-personal.com>
2024-09-23 22:01:29 +0000

    cmd #1: use dynamic memory for managing args

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -xe
 
 CC="gcc"
-FLAGS="-Wall -Wpedantic"
+FLAGS="-Wall -Wpedantic -ggdb"
 SRCS="$(find ./src -type f -name "*.c")"
 CURRENT_DIR="$(git rev-parse --show-toplevel)"
 BUILD_DIR="${CURRENT_DIR}/build"
