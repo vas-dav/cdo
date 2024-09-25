5e932af72d2cbaeabbe74f283cfd18eb2781ce49
tylen <tylen@vm-personal.com>
2024-09-25 19:41:00 +0000

    platform: get full path of the project
    issue #2

--- src/cdo.c
+++ src/cdo.c
@@ -4,6 +4,7 @@
 
 #include "cmd.h"
 #include "tools/log.h"
+#include "platform/operations.h"
 
 static void __attribute__((constructor)) init_cdo(void) {
   atexit(exit_cmd);
@@ -13,7 +14,7 @@ static void __attribute__((constructor)) init_cdo(void) {
 
 int main (int argc, char *argv[]) {
   struct ArgList* args = parse_args(argc, argv);
-  const char* project_path = extract_value_from_arg(args, ARG_PROJECT);
-  LOG_INFO("Project full path: %s", project_path);
+  const char* relative_project_path = extract_value_from_arg(args, ARG_PROJECT);
+  LOG_INFO("Project full path: %s", convert_relative_to_full_path(relative_project_path));
   return 0;
 }
