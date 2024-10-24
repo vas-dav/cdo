b67f2cf13a987255fe5b6ce79e133ffba0d3844c
tylen <tylen@vm-personal.com>
2024-10-24 20:45:10 +0000

    src: init project module

---
+++ src/project/files.c
@@ -0,0 +1,5 @@
+#include "files.h"
+
+Lines* get_ascii_text_files_from_root(const char* root) {
+    return NULL;
+}
---
+++ src/project/files.h
@@ -0,0 +1,8 @@
+#ifndef CDO_FILES_H_
+#define CDO_FILES_H_
+
+#include "../tools/buffer.h"
+
+Lines* get_ascii_text_files_from_root(const char* root);
+
+#endif // CDO_FILES_H_
